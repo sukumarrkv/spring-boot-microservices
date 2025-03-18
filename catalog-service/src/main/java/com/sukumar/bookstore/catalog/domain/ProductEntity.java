17113e6be3b5545c452078a7c908b828494178ab
sukumarrkv <kypa.sukumar3@gmail.com>
2025-03-18 11:33:44 +0530

    Added new packages and classes in catalog service

---
+++ catalog-service/src/main/java/com/sukumar/bookstore/catalog/domain/ProductEntity.java
@@ -0,0 +1,10 @@
+package com.sukumar.bookstore.catalog.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class ProductEntity {
+
+}
---
+++ catalog-service/src/main/java/com/sukumar/bookstore/catalog/web/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.sukumar.bookstore.catalog.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+}
