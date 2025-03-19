dc4d693f99f0cf64a8b5ad7f3cc0328207f6eb21
sukumarrkv <kypa.sukumar3@gmail.com>
2025-03-19 11:33:20 +0530

    Added product service and product repository

---
+++ catalog-service/src/main/java/com/sukumar/bookstore/catalog/domain/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sukumar.bookstore.catalog.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long>{
+
+}
---
+++ catalog-service/src/main/java/com/sukumar/bookstore/catalog/domain/ProductService.java
@@ -0,0 +1,8 @@
+package com.sukumar.bookstore.catalog.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
