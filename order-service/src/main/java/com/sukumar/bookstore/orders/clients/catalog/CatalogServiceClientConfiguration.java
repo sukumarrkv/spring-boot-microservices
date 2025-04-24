2f1ff6f3a6ff3ef82b57b4b405857e2a4386dfd2
sukumarrkv <kypa.sukumar3@gmail.com>
2025-04-24 11:37:34 +0530

    Implemented time limiter and retry resiliance patterns in order service

--- order-service/src/main/java/com/sukumar/bookstore/orders/clients/catalog/CatalogServiceClientConfiguration.java
+++ order-service/src/main/java/com/sukumar/bookstore/orders/clients/catalog/CatalogServiceClientConfiguration.java
@@ -1,7 +1,11 @@
 package com.sukumar.bookstore.orders.clients.catalog;
 
+import java.time.Duration;
+
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import com.sukumar.bookstore.orders.domain.ApplicationProperties;
@@ -11,6 +15,16 @@ public class CatalogServiceClientConfiguration {
 	
 	@Bean
 	RestClient restClient(ApplicationProperties properties) {
-		return RestClient.builder().baseUrl(properties.catalogServiceUrl()).build();
+		//Time limiter on Rest client itself instead of using resilience4j as it using  Completable future
+		//and it is complex.
+		ClientHttpRequestFactory clientHttpRequestFactory = ClientHttpRequestFactoryBuilder.simple()
+				.withCustomizer((customizer) -> {
+					customizer.setConnectTimeout(Duration.ofSeconds(5));
+					customizer.setReadTimeout(Duration.ofSeconds(5));
+				}).build();
+		
+		return RestClient.builder().baseUrl(properties.catalogServiceUrl())
+				.requestFactory(clientHttpRequestFactory)
+				.build();
 	}
 }
--- order-service/src/main/java/com/sukumar/bookstore/orders/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/sukumar/bookstore/orders/clients/catalog/ProductServiceClient.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
 @Component
 public class ProductServiceClient {
 
@@ -17,15 +19,19 @@ public class ProductServiceClient {
 		this.restClient = restClient;
 	}
 
+	@Retry(name = "catalog-service", fallbackMethod = "getProductByCodeFallBackMethod")
 	public Optional<Product> getProductByCode(String productCode) {
-		try {
-			LOGGER.info("Getting product with code :" + productCode);
-			Product product = restClient.get().uri("/api/products/{code}", productCode).retrieve().body(Product.class);
-			return Optional.ofNullable(product);
-		} catch (Exception e) {
-			LOGGER.info("Error occurred while getting product with code :" + productCode);
-			LOGGER.error("Error occurred while getting product with code :" + e.getMessage());
-			return Optional.empty();
-		}
+		LOGGER.info("Getting product with code :" + productCode);
+		Product product = restClient.get().uri("/api/products/{code}", productCode).retrieve().body(Product.class);
+		return Optional.ofNullable(product);
+	}
+	
+	public Optional<Product> getProductByCodeFallBackMethod(String productCode, Throwable t) {
+		//We can pass same arguments from method above with Throwable as one more extra argument, this throwable will be used if we to 
+		//handle fall back mechanism based on the exceptions
+		//One more important is as a developer we should not assume this is right fall back mechanism, we should always get this fallback mechanism
+		//from business people. In out case we just return optional of empty, so that order is not placed.
+		LOGGER.info("Fall back method executed for getProductByCode method for product with code: "+ productCode);
+		return Optional.empty();
 	}
 }
