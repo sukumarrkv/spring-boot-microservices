d8126064ee1e40a138215af4f91e4e8646a0812e
sukumarrkv <kypa.sukumar3@gmail.com>
2025-06-26 11:41:48 +0530

    Used Keycloak container to run integration tests

--- order-service/src/test/java/com/sukumar/bookstore/orders/AbstractIntegrationTest.java
+++ order-service/src/test/java/com/sukumar/bookstore/orders/AbstractIntegrationTest.java
@@ -1,18 +1,28 @@
 package com.sukumar.bookstore.orders;
 
 import java.math.BigDecimal;
+import java.util.Collections;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.keycloak.OAuth2Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.wiremock.integrations.testcontainers.WireMockContainer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.tomakehurst.wiremock.client.WireMock;
 
 import io.restassured.RestAssured;
@@ -21,6 +31,9 @@ import io.restassured.RestAssured;
 @Import(TestcontainersConfiguration.class)
 public class AbstractIntegrationTest {
 
+    @Autowired
+    OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
+    
 	@LocalServerPort
 	private int port;
 	
@@ -55,4 +68,30 @@ public class AbstractIntegrationTest {
 								""".formatted(code, name, price.doubleValue())
 								)));
 	}
+	
+	protected String getToken() {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		map.put(OAuth2Constants.GRANT_TYPE, Collections.singletonList(OAuth2Constants.PASSWORD));
+		map.put(OAuth2Constants.CLIENT_ID, Collections.singletonList(""));
+		map.put(OAuth2Constants.CLIENT_SECRET, Collections.singletonList(""));
+		map.put(OAuth2Constants.USERNAME, Collections.singletonList(""));
+		map.put(OAuth2Constants.PASSWORD, Collections.singletonList(""));
+		
+		String authServerUrl = oAuth2ResourceServerProperties.getJwt().getIssuerUri() + "/protocol/openid-connect/token";
+		
+		var request = new HttpEntity<>(map, httpHeaders);
+		
+		KeyCloakToken token = restTemplate.postForObject(authServerUrl, request, KeyCloakToken.class);
+		assert token != null;
+		return token.accessToken();
+		
+	}
+	
+	record KeyCloakToken(
+			@JsonProperty("access_token")
+			String accessToken
+			) {}
 }
--- order-service/src/test/java/com/sukumar/bookstore/orders/TestcontainersConfiguration.java
+++ order-service/src/test/java/com/sukumar/bookstore/orders/TestcontainersConfiguration.java
@@ -3,13 +3,20 @@ package com.sukumar.bookstore.orders;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
+import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
+	private static final String KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:26.0";
+	private static final String REALM_IMPORT_FILE_PATH = "";
+	private static final String REALM_NAME = "bookstore";
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
@@ -22,4 +29,11 @@ class TestcontainersConfiguration {
 		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.13.7-management"));
 	}
 
+	@Bean
+	KeycloakContainer keycloakContainer(DynamicPropertyRegistry registry) {
+		KeycloakContainer keycloakContainer = new KeycloakContainer(KEYCLOAK_IMAGE).withRealmImportFile(REALM_IMPORT_FILE_PATH);
+		registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> keycloakContainer.getAuthServerUrl()+"/realms/"+REALM_NAME);
+		return keycloakContainer;
+		
+	}
 }
