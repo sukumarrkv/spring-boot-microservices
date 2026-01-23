d4a6ce71cb1d594e270b931a648bc78cc12573ff
sukumarrkv <kypa.sukumar3@gmail.com>
2026-01-23 11:31:45 +0530

    Fixed issue with view not updating when query params change

--- bookstore-ui/src/app/product/pagination/pagination.component.ts
+++ bookstore-ui/src/app/product/pagination/pagination.component.ts
@@ -10,23 +10,8 @@ import { RouterLink } from "@angular/router";
   styleUrl: './pagination.component.css',
 })
 export class PaginationComponent {
-  // productResponse: ProductResponse = {
-  //   data: [],
-  //   totalElements: 0,
-  //   pageNumber: 0,
-  //   totalPages: 0,
-  //   isFirst: true,
-  //   isLast: false,
-  //   hasNext: true,
-  //   hasPrevious:  false
-
-  // };
-  private productService = inject(ProductService);
-  //pageNumber: string = '0';
-  page = input<string>('0')
-
   @Input() productResponse: ProductResponse = {
-        data: [],
+    data: [],
     totalElements: 0,
     pageNumber: 0,
     totalPages: 0,
@@ -35,14 +20,4 @@ export class PaginationComponent {
     hasNext: true,
     hasPrevious:  false
   }
-
-  // ngOnInit(): void {
-  //   console.log(this.page());
-  //   this.productService.loadProducts(this.page()).subscribe({
-  //     next: (response) => {
-  //       this.productResponse = response;
-  //       console.log("ProductResponse", this.productResponse);
-  //     }
-  //   })
-  // }
 }
--- bookstore-ui/src/app/product/product.component.ts
+++ bookstore-ui/src/app/product/product.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input, OnInit, output, signal } from '@angular/core';
+import { Component, DestroyRef, inject, input, OnInit, output, signal } from '@angular/core';
 import { Product, ProductResponse } from './product.model';
 import { ProductService } from './product.service';
 import { HeaderComponent } from "../core/header/header.component";
 import { PaginationComponent } from "./pagination/pagination.component";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -25,10 +26,23 @@ export class ProductComponent implements OnInit {
     hasNext: true,
     hasPrevious:  false
   });
-
+  private activatedRoute = inject(ActivatedRoute);
+  private destroRef = inject(DestroyRef);
   ngOnInit(): void {
     console.log('Page number', this.page())
-    this.productService.loadProducts(this.page()).subscribe({
+    //this.loadProducts(this.page());
+    const queryParamsSubscription = this.activatedRoute.queryParams.subscribe(params => {
+      console.log("Inside activated route");
+      this.loadProducts(params['page']);
+    })
+
+    this.destroRef.onDestroy(() => {
+      queryParamsSubscription.unsubscribe();
+    })
+  }
+
+  loadProducts(pageNumber: string) {
+    this.productService.loadProducts(pageNumber).subscribe({
       next: (response) => {
         this.productResponse.set(response);
         this.products.set(response.data);
